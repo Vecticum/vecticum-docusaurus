806e8d42809df2a53939e7d84fd560f459e803a1
SarunasMazintas <101090494+SarunasMazintas@users.noreply.github.com>
2024-08-30 09:29:21 +0300

    page-new-url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://vecticum.github.io/',
+  url: 'https://doc.vecticum.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vecticum-docusaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
